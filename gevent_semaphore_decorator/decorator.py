36edd2c3abfc0a030a536f715a4cd92715042d11
Calvin French-Owen <calvin@calv.info>
2017-02-15 15:29:59 -0800

    adds logging on number of attempts + timeout
    Adds a "timeout" to turn this into more of a spin-lock. It's to
    try and help with any deadlock issues, and tell us how often
    the lock is attempted to be acquired.

--- gevent_semaphore_decorator/decorator.py
+++ gevent_semaphore_decorator/decorator.py
@@ -1,21 +1,29 @@
 from functools import wraps
+import logging
 
 from gevent.lock import BoundedSemaphore
 
 
-def semaphore(size):
+def semaphore(size, timeout=30):
     sem = BoundedSemaphore(size)
 
     def decorator(func):
         @wraps(func)
         def func_wrapper(*args, **kwargs):
-            try:
-                sem.acquire()
-                result = func(*args, **kwargs)
-            finally:
-                sem.release()
-
-            return result
+            tries = 0
+            while True:
+                acquired = sem.acquire(timeout=timeout)
+                if acquired:
+                    try:
+                        logging.info("acquired semaphore: %s, attempts: %s",
+                            func, tries)
+                        result = func(*args, **kwargs)
+                    finally:
+                        sem.release()
+                    return result
+                tries += 1
+                logging.info("retrying semaphore: %s, attempts: %s",
+                    func, tries)
 
         return func_wrapper
 
